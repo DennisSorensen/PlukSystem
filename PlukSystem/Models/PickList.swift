d366f971dcd1b057a50b512cc46b1eed5b60e89e
Dennis Sørensen <dennissorensen24@hotmail.dk>
2020-04-27 13:41:24 +0200

    Adding models

---
+++ PlukSystem/Models/PickList.swift
@@ -0,0 +1,33 @@
+//
+//  PickList.swift
+//  PlukSystem
+//
+//  Created by Dennis Sørensen on 27/04/2020.
+//  Copyright © 2020 Dennis Sørensen. All rights reserved.
+//
+
+import SwiftUI
+
+struct PickList: Identifiable {
+    public let id = UUID()
+    public let salesOrder : SalesOrder
+    public var toBePicked = 0
+    
+    init(salesOrder: SalesOrder, toBePicked: Int) {
+        self.salesOrder = salesOrder
+        self.toBePicked = toBePicked
+    }
+}
+
+
+//DEMO DATA
+extension PickList {
+    static let demoData = [
+        PickList(salesOrder: SalesOrder(id: "4711"), toBePicked: 11),
+        PickList(salesOrder: SalesOrder(id: "4712"), toBePicked: 1),
+        PickList(salesOrder: SalesOrder(id: "4713"), toBePicked: 16),
+        PickList(salesOrder: SalesOrder(id: "4714"), toBePicked: 5),
+        PickList(salesOrder: SalesOrder(id: "4715"), toBePicked: 3),
+        PickList(salesOrder: SalesOrder(id: "4716"), toBePicked: 141),
+    ]
+}
---
+++ PlukSystem/Models/SalesOrder.swift
@@ -0,0 +1,15 @@
+//
+//  SalesOrder.swift
+//  PlukSystem
+//
+//  Created by Dennis Sørensen on 27/04/2020.
+//  Copyright © 2020 Dennis Sørensen. All rights reserved.
+//
+
+import SwiftUI
+
+//Not expecting to show a list of salesorders
+//Not implementing Identifiable
+struct SalesOrder {
+    var id: String
+}
